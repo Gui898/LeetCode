42a7ce5907a8c030b9484806f22fa5d5081ef077
Gui898 <gui.miojo.lasanha@gmail.com>
2025-12-17 16:14:13 -0300

    TwoSum

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
+        int[] arr = {2,7,11,15};
+        System.out.println(Arrays.toString(twoSum(arr, 9)));;
+    }
 
+    public static int[] twoSum(int[] nums, int target) {
+        int i = 0;
+        int j = i+1;
+        while(i != nums.length){
 
+            if(j < nums.length && nums[i] + nums[j] == target) return new int[]{i, j};
+            else j++;
 
+            if(j >= nums.length){
+                i++;
+                j=i+1;
+            }
+        }
+        return new int[0];
     }
 }
