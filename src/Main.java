0a69d7f3930de2c35e432354fb3d33103440d9ff
Gui898 <gui.miojo.lasanha@gmail.com>
2025-12-21 14:30:47 -0300

    reverseVowels

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,45 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
         int[] arr = {0,1,0,3,12};
-        System.out.println(isSubsequence("abc", "ahbgdc"));
+        System.out.println(reverseVowels("IceCreAm"));
     }
 
-    public static boolean isSubsequence(String s, String t) {
-        int i=0;
-        int j=0;
-        while(i<s.length() && j<t.length()){
-            if(s.charAt(i)==t.charAt(j)) i++;
-            j++;
+    public static String reverseVowels(String s) {
+
+        char[] chars = s.toCharArray();
+
+        int i = 0 ;
+        int j = s.length()-1;
+
+        while (i< j){
+            while (i < j && !isVowel(chars[i])) {
+                i++;
+            }
+
+            while (i < j && !isVowel(chars[j])) {
+                j--;
+            }
+
+            if (i < j) {
+                swap(chars, i, j);
+                i++;
+                j--;
+            }
+
         }
-        return i==s.length();
+
+        return new String(chars);
+    }
+
+    private static void swap(char[] word, int start, int end){
+        char temp = word[start];
+        word[start] = word[end];
+        word[end] = temp;
+    }
+
+    private static boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+                || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
 }
