a87b9817be02c85f1914b9e750530a9420b57467
Gui898 <gui.miojo.lasanha@gmail.com>
2026-03-09 20:20:50 -0300

    lengthOfLastWord

--- src/Main.java
+++ src/Main.java
@@ -8,26 +8,32 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {2,3,5,1,3};
 
-        System.out.println(kidsWithCandies(arr, 3));
+        System.out.println(lengthOfLastWord("   fly me   to   the moon  "));
+
     }
 
-    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        int max = 0;
-        for(int a : candies){
-            if(a > max){
-                max = a;
+    public static int lengthOfLastWord(String s) {
+        int size = s.length();
+        boolean hasWord = false;
+        int val = 0;
+
+        if(size == 0) return 0;
+
+        for (int i = size-1; i >= 0; i--) {
+
+            if(s.charAt(i) != ' '){
+                val++;
+                hasWord = true;
+            }else{
+                hasWord = false;
+            }
+
+            if(val != 0 && !hasWord){
+                break;
             }
         }
 
-        List<Boolean> list = new LinkedList<>();
-
-        for(int a : candies){
-            if(a+extraCandies >= max){
-                list.add(true);
-            }else list.add(false);
-        }
-
-        return list;
+        return val;
     }
 
 
