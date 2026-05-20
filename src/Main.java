3731e5523b73292250b22fed5aa4e18c3a252eaf
Gui898 <guihg2718@gmail.com>
2026-05-19 22:53:44 -0300

    reverseWords

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,26 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         int[] arr = {1,0,0,0,0,1};
 
-        System.out.println(mergeAlternately("abcd", "pq"));
+        System.out.println(reverseWords("a good   example"));
 
     }
 
-    public static String mergeAlternately(String word1, String word2) {
+    public static String reverseWords(String s) {
+        String[] arr = s.split(" ");
         StringBuilder ans = new StringBuilder();
 
-        int val = Math.max(word1.length(), word2.length());
-
-        for(int i = 0; i < val; i++){
-            if(i < word1.length()){
-                ans.append(word1.charAt(i));
+        for (int i = arr.length-1; i >= 0; i--) {
+            if (arr[i].isEmpty()){
+                continue;
             }
-            if(i < word2.length()){
-                ans.append(word2.charAt(i));
-            }
+            ans.append(arr[i]);
+            ans.append(" ");
         }
-
-        return ans.toString();
+        return ans.toString().strip();
     }
 
 }
