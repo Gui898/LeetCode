655b9df2531a1afa0b3776df547ab31f3b66eb84
Gui898 <guihg2718@gmail.com>
2026-05-25 19:39:05 -0300

    maxArea

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,34 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {2,1,-1};
+        int[] arr = {1,8,6,2,5,4,8,3,7};
 
-        System.out.println(pivotIndex(arr));
+        System.out.println(maxArea(arr));
     }
 
-    //lado direito menos o lado esquerdo dos prefixos
+    public static int maxArea(int[] height) {
+        int n = height.length;
+        int left = 0;
+        int right = n-1;
 
-    public static int pivotIndex(int[] nums) {
-        int n = nums.length;
-        int[] prefix = new int[n];
+        int area = 0;
+        while (left < right){
+            int minor = Math.min(height[left], height[right]);
 
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            prefix[i] = sum;
-        }
+            int current = (right - left) * minor;
 
-        for (int i = 0; i < n; i++) {
-
-            int val = prefix[n-1] - prefix[i];
-
-            if(i == 0){
-                if(0 == val){
-                    return i;
-                }
-            }else {
-                if(prefix[i-1] == val){
-                    return i;
-                }
+            if(height[left] < height[right]){
+                left++;
+            }else{
+                right--;
             }
 
+            if (current > area){
+                area = current;
+            }
         }
 
-        return -1;
+        return area;
     }
 
 }
