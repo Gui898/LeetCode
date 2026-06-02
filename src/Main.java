1aab607ac319ae37f3b0de43432a9899366c40b1
Gui898 <guihg2718@gmail.com>
2026-06-02 15:01:11 -0300

    flood fill

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,44 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {1,8,6,2,5,4,8,3,7};
+        int[][] arr = {{0,0,0},
+                       {0,0,0}};
 
-        System.out.println(numberOfSteps(123));
+        int[][] matrix = floodFill(arr, 1, 0, 2);
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
-    public static int numberOfSteps(int num) {
-        return helper(num, 0);
-    }
-
-    public static int helper(int n, int counter){
-        if(n == 0 && counter != 0){
-            return counter-1;
-        }else if(n == 0 & counter == 0){
-            return 0;
+    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if(image[sr][sc] == color){
+            return image;
         }
 
-        if(n % 2 == 0){
-            return helper(n / 2, counter+1);
+        helper(image, sr, sc, color, image[sr][sc]);
+
+        return image;
+    }
+
+    public static void helper(int[][] image, int sr, int sc, int color, int initial){
+        if((sr >= 0 && sr < image.length) && (sc >= 0 && sc < image[0].length)){
+            if(image[sr][sc] == initial){
+                image[sr][sc] = color;
+
+                helper(image, sr, sc+1, color, initial);
+                helper(image, sr+1, sc, color, initial);
+                helper(image, sr, sc-1, color, initial);
+                helper(image, sr-1, sc, color, initial);
+            }else{
+                return;
+            }
         }
 
-        return helper(n / 2, counter+2);
+        return;
     }
+
 }
---
+++ src/Studies.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Locale;
+
+public class Studies {
+
+    public static void main(String[] args) {
+        int[] arr = {7,6,2,78,9,2,4,1,3,7,5,9,665,12};
+        
+    }
+
+
+}
