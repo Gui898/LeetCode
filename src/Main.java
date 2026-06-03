7eb75ca213ff097348de294c4a2d195b3e362d20
Gui898 <guihg2718@gmail.com>
2026-06-03 11:01:29 -0300

    LetterCombinationsOnAPhone

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         int[][] arr = {{0,0,0},
                        {0,0,0}};
 
-        int[][] matrix = floodFill(arr, 1, 0, 2);
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
+        System.out.println(letterCombinations("23"));
     }
 
-    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc] == color){
-            return image;
-        }
-
-        helper(image, sr, sc, color, image[sr][sc]);
-
-        return image;
+    public static List<String> letterCombinations(String digits) {
+        return helper("", digits);
     }
 
-    public static void helper(int[][] image, int sr, int sc, int color, int initial){
-        if((sr >= 0 && sr < image.length) && (sc >= 0 && sc < image[0].length)){
-            if(image[sr][sc] == initial){
-                image[sr][sc] = color;
+    static List<String> helper(String p, String up){
 
-                helper(image, sr, sc+1, color, initial);
-                helper(image, sr+1, sc, color, initial);
-                helper(image, sr, sc-1, color, initial);
-                helper(image, sr-1, sc, color, initial);
-            }else{
-                return;
-            }
+        List<String> val = new ArrayList<>();
+
+        if(up.isEmpty()){
+            val.add(p);
+            return val;
         }
 
-        return;
+        int digit = (up.charAt(0) - '0') - 1;
+        int start = (digit-1) * 3;
+        int end = digit * 3;
+
+        if(digit == 6){
+            end += 1;
+        } else if (digit == 7) {
+            start += 1;
+            end += 1;
+        } else if (digit == 8) {
+            start += 1;
+            end += 2;
+        }
+
+        for (int i = start; i < end; i++) {
+            char ch = (char) ('a' + i);
+            val.addAll(helper(p + ch, up.substring(1)));
+        }
+
+        return val;
     }
 
 }
